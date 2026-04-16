dffa80ea598cbd62d3c3cdbbb5f0816ac5ace786
clba4780 <clba4780@colorado.edu>
2026-04-16 11:08:46 -0600

    creating temperature plot

---
+++ project/__init__.py
@@ -0,0 +1,3 @@
+from .core import list_variables, load, open_era5
+
+__all__ = ["open_era5", "list_variables", "load"]
--- project/analysis.py
+++ project/analysis.py
@@ -9,55 +9,36 @@ total cloud cover from ERA-5
 loads the data into seperate variables
 - Use a name gaurd to create a simple plot with each variable to test the function
 """
-
-
 def get_t2m(time_slice, lat, lon):
-    da1 = open_era5(
-        "2m_temperature", 
-        time_slice = time_slice,
-        lat = lat, 
-        lon = lon
-    )
-    t2m = da1.load()
-    return(t2m)
+    return (open_era5("2m_temperature", time_slice=time_slice, lat=lat, lon=lon))
 
 def get_toa(time_slice, lat, lon):
-    da2 = open_era5(
-        "toa_incident_solar_radiation", 
-        time_slice = time_slice,
-        lat = lat,
-        lon = lon
-    )
-    toa = da2.load()
-    return (toa)
+    return (open_era5("toa_incident_solar_radiation", time_slice=time_slice, lat=lat, lon=lon))
 
 def get_cc(time_slice, lat, lon):
-    da3 = open_era5(
-        "total_cloud_cover",
-        time_slice = time_slice, 
-        lat = lat, 
-        lon = lon
-    )
-    cc = da3.load()
-    return(cc)
+    return (open_era5("total_cloud_cover", time_slice=time_slice, lat=lat, lon=lon))
 
-def get_variables(time_slice, lat, lon):
-    t2m = get_t2m(time_slice, lat, lon),
-    cc = get_cc(time_slice, lat, lon), 
-    toa = get_toa(time_slice, lat, lon)
-    return t2m, cc, toa
+def get_era5_variables(time_slice, lat, lon):
+    t2m = load(get_t2m(time_slice, lat, lon))
+    toa = load(get_toa(time_slice, lat, lon))
+    cc = load(get_cc(time_slice, lat, lon))
+    return (t2m, toa, cc)
 
-get_variables(("2020-01-01", "2020-01-31"), (37,40), (95,102))
+t2m, toa, cc = get_era5_variables(
+    time_slice = ("2020-01-01", "2020-01-31"),
+    lat = (37,40),
+    lon = (95, 102)
+)
 
-if __name__ == "__main__":
-    t2m_mean = t2m.mean(["latitude", "longitude"]) - 273.15
+ts = t2m.mean(["latitude", "longitude"]) - 273.15
 
-    fig, ax = plt.subplots(figsize=(12, 4))
-    t2m_mean.plot(ax=ax)
-    ax.set_title("ERA5 2-m Temperature — Colorado box, summer 2010")
-    ax.set_ylabel("Temperature (°C)")
-    ax.set_xlabel("Time")
-    plt.tight_layout()
-    plt.savefig("era5_t2m_colorado.png", dpi=150)
-    print("Saved era5_t2m_colorado.png")
-    plt.show()
+fig, ax = plt.subplots(figsize=(12, 4))
+ts.plot(ax=ax)
+ax.set_title("ERA5 2-m Temperature — Kansas box, January 2020")
+ax.set_ylabel("Temperature (°C)")
+ax.set_xlabel("Time")
+plt.tight_layout()
+plt.savefig("era5_t2m_kansas.png", dpi=150)
+print("Saved era5_t2m_kansas.png")
+plt.show()
+
