e7e55fdf20e3f34117b4cddce208971bf617a6ca
clba4780 <clba4780@colorado.edu>
2026-04-21 15:35:58 -0600

    switching variables

--- grab_era5/data.py
+++ grab_era5/data.py
@@ -1,5 +1,4 @@
 from grab_era5 import list_variables
-from grab_era5 import open_era5
 
 # All 200+ variables
 for v in list_variables():
@@ -7,5 +6,3 @@ for v in list_variables():
 
 # Filter by keyword
 (list_variables("temperature"))
-(list_variables("wind"))
-(list_variables("precipitation"))
--- project/analysis.py
+++ project/analysis.py
@@ -20,20 +20,22 @@ Paramaters:
 """
 
 # A funtion load the date into three different variables
-def get_era5_variables(time_slice, lat, lon, cache = True):
-    fname = f"era_5_{time_slice[0]}_{time_slice[1]}_{lat}_{lon}"
+def get_era5_variables(time_slice, lat, lon, name, cache = True):
+    fname = f"era_5_{name}"
     
     if cache and os.path.exists(fname + ".nc"):
         print ("Loading from cache...")
         return xr.open_dataset(fname + ".nc")
     
     print ("Downloading data set...")
-    t2m = load(open_era5("2m_temperature", time_slice, lat, lon))
+    snsr = load(open_era5("surface_net_solar_radiation", time_slice, lat, lon))
+    snsr_cs = load(open_era5("surface_net_solar_radiation_clear_sky", time_slice, lat, lon))
     toa = load(open_era5("toa_incident_solar_radiation", time_slice, lat, lon))
     cc = load(open_era5("total_cloud_cover",time_slice, lat, lon))
     
     ds = xr.Dataset({
-        "t2m" : t2m, 
+        "snsr" : snsr,
+        "snsr_cs": snsr_cs, 
         "toa" : toa, 
         "cc" : cc
     })
@@ -42,15 +44,16 @@ def get_era5_variables(time_slice, lat, lon, cache = True):
         print ("saving dataset...")
         ds.to_netcdf(fname + ".nc")
     
-    return (ds['t2m'], ds['toa'], ds['cc'])
+    return (ds['snsr'], ds['snsr_cs'], ds['toa'], ds['cc'])
 
 
 # input dates, lat, long for each variable in get_era5_variables
-t2m, toa, cc = get_era5_variables(
-    time_slice = ("2020-01-01", "2020-02-01"),
+ds = get_era5_variables(
+    time_slice = ("2020-01-01", "2020-01-07"),
     # lat, lon correspond to the state of Kansas
     lat = (37,40),
-    lon = (95, 102)
+    lon = (95, 102), 
+    name = 'oneweek_jan_kansas'
 )
 
 """
@@ -62,17 +65,20 @@ plot 2: graph of total cloud cover and TOA radoation
 """
 
 # Turn this into a function that takes spatial averages, them temporal averges, then normalize for comparisons
-def avg_values(t2m, toa, cc):
-# 2-meter temperature avaerged over latitude and longitude
-    t2m_avg = t2m.mean(["latitude", "longitude"]) - 273.15
+def avg_values(snsr, snsr_cs, toa, cc):
+# Surface net solar radiation averaged over latitude and longitude
+    snsr_avg = ds['snsr'].mean(["latitude", "longitude"])
+
+#Surface net solar radition clear sky averaged over latutude and longitude
+    snsr_cs_avg = ds['snsr_cs'].mean(['latitude', 'longitude'])
 
 # TOA radiation averaged over lat/lon
-    avg_toa = toa.mean(["latitude", "longitude"])
+    avg_toa = ds['toa'].mean(["latitude", "longitude"])
 
 # total cloud cover averaged over latitude, longitude
-    avg_cc = cc.mean(["latitude", "longitude"])
+    avg_cc = ds['cc'].mean(["latitude", "longitude"])
 
-    return(t2m_avg, avg_toa, avg_cc)
+    return(snsr_avg, snsr_cs_avg, avg_toa, avg_cc)
 
 
 def daily_mean(ds):
@@ -89,31 +95,33 @@ def simple_comparison(time_slice, ds1, ds2, label1, label2, title, fig_name):
     ax1.set_xlabel("Time")
     ax1.set_ylabel(label1, color = 'tab:blue')
     ax1.tick_params(axis = 'y', labelcolor = 'tab:blue')
+    ax1.legend()
 
     ax2 = ax1.twinx()
     ax2.plot(time_slice, ds2, label = label2, color = 'tab:red')
     ax2.set_ylabel(label2, color = 'tab:red')
     ax2.tick_params(axis = 'y', labelcolor = 'tab:red')
+    ax2.legend()
 
     plt.title(title)
-
     plt.savefig(f"{fig_name}.png", dpi = 150)
     plt.show()
 
 
 
 if __name__ == "__main__":
-    t2m_avg, toa_avg, cc_avg = avg_values(t2m, toa, cc)      
+    snsr_avg, snsr_cs_avg, toa_avg, cc_avg = avg_values(ds['snsr'], ds['snsr_cs'], ds['toa'], ds['cc'])      
     
-    t2m_daily = daily_mean(t2m_avg)
+    snsr_daily = daily_mean(snsr_avg) / 1000
+    snsr_cs_daily = daily_mean(snsr_cs_avg) / 1000
     cc_daily = daily_mean(cc_avg)
     toa_daily = daily_mean(toa_avg)
 
-    corr_temp_cloud = correlation(t2m, cc)
-    corr_temp_toa = correlation(t2m, toa)
-    corr_toa_cloud = correlation(toa, cc)
+    corr_temp_cloud = correlation(ds['snsr'], ds['cc'])
+    corr_temp_toa = correlation(ds['snsr'], ds['toa'])
+    corr_toa_cloud = correlation(ds['toa'], ds['cc'])
 
-    simple_comparison(t2m_daily['time'], t2m_daily, toa_daily, 
-                      'Surface Temperature (degC)', 'TOA Incident Solar Radiation (W/m^2)',
-                      'TOA Incident Solar Radition vs 2-meter temperature',
-                      "t2m_vs_ta.png")
+    simple_comparison(snsr_daily['time'], snsr_daily, snsr_cs_daily, 
+                      'Surface Net Solar Radition (W/km^2)', 'Surface Net Solar Radiation Clear Sky (W/km^2)',
+                      'Surface Net Solar Radtion vs. Clear Sky Surface Net Solar Raditation',
+                      "snsr_vs_snsrcs.png")
