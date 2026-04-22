caf08715133735019ba78eb36e66af4b1601bfb0
clba4780 <clba4780@colorado.edu>
2026-04-22 09:54:21 -0600

    comparing cloud cover and surface solar radiation

--- project/relationship.py
+++ project/relationship.py
@@ -1,46 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-"""
-from project.analysis import get_era5_variables
+from analysis import get_era5_variables
 
 ds = get_era5_variables(
-    time_slice= ("2026-01-01", "2026-01-02"),
-    lat = (37,40), 
-    lon = (95,102)
+    time_slice= ("2025-06-01", "2025-08-31"),
+    lat = (25,50), 
+    lon = (-125, 165),
+    name = "era5_summer2025_contigUS"
 )
 
-t2m = ds["t2m"]
-toa = ds["toa"]
-cc = ds["cc"]
-
-"""
-
-""" Synthetic Tester Data 
-Goal: figure out how to import from my data then
- take spatial and temporal avg and plot"""
-# Time axis (e.g., 3 days hourly)
-n = 72
-time = np.arange(n)
-
-# Simulate TOA radiation (peaks during "day", zero at "night")
-toa = np.maximum(0, 800 * np.sin(2 * np.pi * time / 24))
-
-# Simulate cloud cover (random but somewhat smooth)
-np.random.seed(0)
-cc = np.clip(0.5 + 0.3 * np.sin(2 * np.pi * time / 24 + 1) + 0.2 * np.random.randn(n), 0, 1)
-
-# Simulate temperature:
-# depends on radiation, reduced by clouds, plus some lag + noise
-t2m = 0.02 * toa * (1 - cc) + 2 * np.sin(2 * np.pi * (time - 3) / 24) + np.random.randn(n)
+# scatter plot for cloud cover and sw surface radiation
+cc = ds['cc'].mean(dim = 'time')
+snsr = ds['snsr'].mean(dim = 'time')
 
 
-
-
-plt.scatter(toa, t2m, c=cc, cmap='viridis')
-plt.xlabel("TOA Incident Solar Radiation (W/m^2)")
-plt.ylabel("2-meter Temperature (degC)")
+plt.scatter(cc, snsr, alpha = 0.3)
+plt.xlabel("Total Cloud Cover")
+plt.ylabel("Surface Net Solar Radiation")
 plt.colorbar(label = "Cloud Cover")
-plt.title("TOA Incident Solar Radiation vs 2-meter temperture")
+plt.title("Cloud Cover vs Solar Radiation")
 
 plt.savefig("relationship.png", dpi = 150)
 plt.show()
