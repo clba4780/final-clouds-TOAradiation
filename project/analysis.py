6801d74c4ab04d08c0efcffda03880aa217f0e22
clba4780 <clba4780@colorado.edu>
2026-04-23 09:12:27 -0600

    mapping

--- project/analysis.py
+++ project/analysis.py
@@ -66,32 +66,32 @@ def get_era5_variables(time_slice, lat, lon, name, cache = True):
 
 
 ds = get_era5_variables(
-    time_slice = ("2025-06-22", "2025-06-22"),
+    time_slice = ("2025-06-01", "2025-08-31"),
     # lat, lon correspond to the state of Kansas
     lat = (25, 50),
     lon = (-125, -65), 
-    name = '2025_Jun22_contigUS'
+    name = 'era_5_summer2025_contigUS'
 )
 
 # take the difference between snsr and snsr_cs to determine the radiative effect of the cloud
-ds['cre'] = ds['snsr_cs'] - ds['snsr']
+ds['cre'] = (ds['snsr_cs'] - ds['snsr'])/1000
 
 # take the ratio to determine the efficient the solar radiation is
-ds['efficiency'] = ds['snsr']/ds['snsr_cs']
+ds['efficiency'] = (ds['snsr']/ds['snsr_cs'])/1000
 
 
-def mapping(title, ):
+def mapping(title, filename, label, ds):
     fig, ax = plt.subplots(
         figsize=(10, 5),
         subplot_kw={'projection': ccrs.Robinson()}   # 1. Choose map projection
     )
 
     # 2. Plot xarray data — always set transform to your data's CRS
-    ds['cre'].isel(time=0).plot(
+    ds['efficiency'].isel(time=0).plot(
         ax=ax,
         transform=ccrs.PlateCarree(),                # data is on regular lat/lon
         cmap='plasma',
-        cbar_kwargs={'label': 'Cloud Radiative Effect', 'shrink': 0.7}
+        cbar_kwargs={'label': label, 'shrink': 0.7}
     )
 
     # 3. Add geographic features
@@ -100,7 +100,16 @@ def mapping(title, ):
     ax.add_feature(cfeature.LAND, facecolor='lightgray', alpha=0.3)
     ax.gridlines(draw_labels=True, linewidth=0.5, alpha=0.5)
 
-    ax.set_title('Cloud Radiative Effect — Model Day 1', fontsize=13)
+    # Add state borders
+    ax.add_feature(cfeature.STATES, edgecolor = 'gray', linewidth = 0.5, alpha = 0.2)
+
+
+    ax.set_title(title, fontsize=13)
     plt.tight_layout()
-    plt.savefig()
+    plt.savefig(filename, dpi=150)
+    print (f"{filename} saved")
     plt.show()
+
+    return fig,ax
+
+mapping("Cloud Efficiency Summer 2025", "efficiency_contigUS_summer.png","Cloud Efficiency (0-1)" ,ds)
